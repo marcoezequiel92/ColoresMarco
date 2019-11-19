e5bc8af11e15827c20303ec180f36d1f6c3249a6
marcoezequiel92 <marcoezequielsuarez92@gmail.com>
2019-11-19 20:29:40 -0300

    Primera versión

---
+++ src/coloresmarco/ColoresMarco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coloresmarco;
+
+/**
+ *
+ * @author b3
+ */
+public class ColoresMarco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2 = "ROJO";
+        color3 = "VERDE";
+        System.out.printf("%s es mi primer color favorito\n", color1);
+        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s es mi tercer color favorito\n", color3);
+    }
+    
+}
